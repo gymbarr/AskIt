d4c2b5550111512841fec1ddb1c36dfea1f39c3f
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-14 22:11:41 +0300

    add answer model and table, association for question-answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_084844) do
+ActiveRecord::Schema.define(version: 2022_08_14_184007) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
