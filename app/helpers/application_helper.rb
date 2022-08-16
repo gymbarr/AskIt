39c54966c0ec017ebe38d6d039abc4e200da6874
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-16 23:22:12 +0300

    add displaying pages only for 2 or more pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def format_timestamp(timestamp, format = '%d-%m-%Y %H:%M:%S')
     timestamp.strftime(format)
   end
+
+  def pagination(obj)
+    raw(pagy_bootstrap_nav(obj)) if obj.pages > 1
+  end
 end
