5a14105d969e55ab6dd40ac6bfd04bd5a58712e3
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-16 13:29:03 +0300

    add editing and deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_question
   before_action :set_answer, only: %i[create]
-  before_action :set_question, only: %i[create]
+  before_action :set_comment, except: %i[create]
 
   def create
     comment = @answer.comments.build(comment_params)
@@ -11,11 +12,37 @@ class CommentsController < ApplicationController
       render 'questions/show'
     end
   end
+
+  def edit
+    @answer = @comment.answer
+    @comments = @answer.comments
+
+    respond_to do |format|
+      format.js { render partial: 'questions/comment_form' }
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to question_path(@question), notice: 'Comment was successfully updated!'
+    else
+      render 'questions/show'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to question_path(@question), notice: 'Comment was successfully deleted!'
+  end
   
   private
   
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
   
   def set_answer
