a8cc8e7423debb0c21510c377f96789e5f4fda9d
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-17 13:07:17 +0300

    add sessions controller, sign in and sign out actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to questions_path, notice: "You've successfully signed up!"
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
 
   resources :questions do
     resources :answers, except: %i[new index show]
