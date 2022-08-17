4057768c0a6ce50a42b13fe6c011384d1d66fae8
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-17 11:20:33 +0300

    add user model, routes, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  resources :users, only: %i[new create]
+
   resources :questions do
     resources :answers, except: %i[new index show]
     resources :comments, except: %i[new index show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_151530) do
+ActiveRecord::Schema.define(version: 2022_08_17_075807) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2022_08_15_151530) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
