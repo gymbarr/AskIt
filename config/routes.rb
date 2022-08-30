c5931569d5d94cd3069b492b360ca78282dcc61d
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-30 15:23:51 +0300

    optimize routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,6 @@ Rails.application.routes.draw do
     resource :session, only: %i[new create destroy]
 
     resources :questions do
-      resources :answers, except: %i[new index show]
-      resources :comments, except: %i[index show]
-
       get 'vote_up', to: 'questions#vote_up'
       get 'vote_down', to: 'questions#vote_down'
     end
@@ -21,9 +18,6 @@ Rails.application.routes.draw do
       get 'vote_up', to: 'replies#vote_up'
       get 'vote_down', to: 'replies#vote_down'
     end
-
-    # get 'answers/:id/vote_up', to: 'answers#vote_up', as: 'answer_vote_up'
-    # get 'answers/:id/vote_down', to: 'answers#vote_down', as: 'answer_vote_down'
 
     resources :categories do
       resources :subscriptions, only: %i[create destroy]
