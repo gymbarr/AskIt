de812c87bbf285fd26b092a63676f37c9c7ea3f2
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-01 18:46:50 +0300

    add tests in user_mailer for locales in subject

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -6,21 +6,31 @@ end
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe '#notify_subscriber', :fast do
-    context 'instant mail sending' do
-      let(:category) { create :category }
-      let(:question) { create :question }
-      let(:user) { question.user }
-      let(:mail) do
-        described_class.with(user: user, category: category, question: question).notify_subscriber.deliver_now
-      end
+  subject(:category) { create :category }
+  subject(:question) { create :question }
+  subject(:user) { question.user }
+  subject(:mail) do
+    described_class.with(user: user, category: category, question: question).notify_subscriber.deliver_now
+  end
 
+  describe '#notify_subscriber', :fast do
+    context 'when instant mail sending' do
       it 'renders the receiver emailname' do
         expect(mail.to[0]).to eq(user.email)
       end
 
-      it 'renders the subject' do
-        expect(mail.subject).to eq("New question in the #{category.name} category! | AskIt")
+      context 'when locale set to en' do
+        it 'renders the subject for en locale' do
+          I18n.locale = :en
+          expect(mail.subject).to eq("New question in the #{category.name} category! | AskIt")
+        end
+      end
+
+      context 'when locale set to ru' do
+        it 'renders the subject for ru locale' do
+          I18n.locale = :ru
+          expect(mail.subject).to eq("Новый вопрос в категории #{category.name}! | AskIt")
+        end
       end
 
       it 'should send an email' do
