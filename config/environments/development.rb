548eb2f18892d5a6ebdeff44d46255d91448c832
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-03 14:51:36 +0300

    add dot env gem for gmail creds, smtp settings for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,15 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV.fetch('GMAIL_USERNAME', 'username'),
+    password: ENV.fetch('GMAIL_SECURED_PASSWORD', 'password'),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
