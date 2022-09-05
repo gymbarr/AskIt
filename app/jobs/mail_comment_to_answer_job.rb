abedfa81164680046e101ce0e87c4f1ce822a762
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-05 16:37:41 +0300

    add jobs for new mailers, mailers modified

---
+++ app/jobs/mail_comment_to_answer_job.rb
@@ -0,0 +1,7 @@
+class MailCommentToAnswerJob < ApplicationJob
+  queue_as :default
+
+  def perform(question, comment)
+    CommentMailer.with(question: question, comment: comment).new_comment_to_answer.deliver_now
+  end
+end
--- app/jobs/new_comment_to_comment_mail_job.rb
+++
@@ -1,7 +0,0 @@
-class NewCommentToCommentMailJob < ApplicationJob
-  queue_as :default
-
-  def perform(question, comment)
-    NewCommentMailer.with(question: question, comment: comment).new_comment_to_comment.deliver_later
-  end
-end
