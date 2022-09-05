ae9f803fa658ee89e40535fe66c2835d7d961af4
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-05 14:30:40 +0300

    localize mails for divided mailers

--- app/jobs/notify_subscriber_job.rb
+++ app/jobs/notify_subscriber_job.rb
@@ -2,7 +2,21 @@ class NotifySubscriberJob < ApplicationJob
   queue_as :default
 
   def perform(params)
-    SubscriptionMailer.with(user: params[:subscriber], category: params[:category], question: params[:question])
+    SubscriptionMailer.with(user: subscriber, category: category, question: question)
                       .new_question_in_category.deliver_later
+  end
+
+  private
+
+  def subscriber
+    params[:subscriber]
+  end
+
+  def category
+    params[:category]
+  end
+
+  def question
+    params[:question]
   end
 end
