3ff05d4e76849f798e2981d5f0bd97181e66abc5
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-05 11:39:33 +0300

    refactoring answer and comment models, factories

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe UserMailer, type: :mailer do
+  describe '#notify_subscriber' do
+    subject(:user) { create :user }
+    subject(:category) { create :category }
+    subject(:question) { create :question }
+    subject(:mail) do
+      described_class.with(user: user, category: category, question: question).notify_subscriber.deliver_now
+    end
+
+    describe '#deliever_now' do
+      it 'renders the receiver emailname' do
+        expect(mail.to[0]).to eq(user.email)
+      end
+
+      context 'when locale set to en' do
+        it 'renders the subject for en locale' do
+          I18n.locale = :en
+          expect(mail.subject).to eq("New question in the #{category.name} category! | AskIt")
+        end
+      end
+
+      context 'when locale set to ru' do
+        it 'renders the subject for ru locale' do
+          I18n.locale = :ru
+          expect(mail.subject).to eq("Новый вопрос в категории #{category.name}! | AskIt")
+        end
+      end
+
+      it 'should send an email' do
+        mail
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+      end
+    end
+
+    describe '#deliever_later' do
+      it 'enqueues email sending to a background job' do
+        expect do
+          described_class.notify_subscriber.deliver_later
+        end.to have_enqueued_job(ActionMailer::MailDeliveryJob)
+      end
+    end
+  end
+
+  describe '#notify_new_reply', :fast do
+    let!(:question) { create :question }
+    let!(:reply) { create :answer }
+    # let!(:mail) do
+    #   described_class.with(question: question, reply: reply).notify_new_reply.deliver_now
+    # end
+
+    describe '#deliever_now', :slow do
+      it 'renders the receiver emailname' do
+       debugger
+      end
+    end
+  end
+end
