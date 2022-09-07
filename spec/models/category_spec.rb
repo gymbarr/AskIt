fdf05f569edca33794c4f80cfec686632ee6f5a4
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-07 19:11:47 +0300

    add tests for models answer, category, comment, question, user

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { build :category }
+  let(:category2) { create :category }
+
+  it 'is valid with valid attributes' do
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    category.name = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'is not valid without a unique name' do
+    category.name = category2.name
+    expect(category).to_not be_valid
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { create :category }
+  let(:question) { build :question, category_ids: [category.id] }
+
+  it 'is valid with valid attributes' do
+    expect(question).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    question.title = nil
+    expect(question).to_not be_valid
+  end
+
+  it 'is not valid without a body' do
+    question.body = nil
+    expect(question).to_not be_valid
+  end
+
+  it 'is not valid without a category' do
+    question.category_ids = nil
+    expect(question).to_not be_valid
+  end
 end
