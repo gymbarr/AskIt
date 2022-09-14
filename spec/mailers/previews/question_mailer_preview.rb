e2635cf9fad83d3c9e7f73406e2ebca3c835c876
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-14 19:09:49 +0300

    add mailers previews

--- spec/mailers/previews/question_mailer_preview.rb
+++ spec/mailers/previews/question_mailer_preview.rb
@@ -1,4 +1,15 @@
 # Preview all emails at http://localhost:3000/rails/mailers/question_mailer
 class QuestionMailerPreview < ActionMailer::Preview
+  def notify_subscriber_about_new_question_in_category
+    question = Question.first
+    user = User.first
+    categories = question.categories
+    categories_name = categories.map(&:name).join(', ')
 
+    QuestionMailer.with(question: question,
+                        user: user,
+                        categories: categories,
+                        categories_name: categories_name)
+                  .notify_subscriber_about_new_question_in_category
+  end
 end
