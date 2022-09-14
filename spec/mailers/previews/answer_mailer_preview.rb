28b42ac95cb571eb1e817995f5ba140ebfcdfebe
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-14 19:09:49 +0300

    add mailers previews

--- spec/mailers/previews/answer_mailer_preview.rb
+++ spec/mailers/previews/answer_mailer_preview.rb
@@ -1,4 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/answer_mailer
 class AnswerMailerPreview < ActionMailer::Preview
+  def notify_user_about_new_answer
+    question = Question.first
+    user = User.first
+    replier = User.last
 
+    AnswerMailer.with(question: question,
+                      user: user,
+                      replier: replier)
+                .notify_user_about_new_answer
+  end
 end
--- spec/mailers/previews/comment_mailer_preview.rb
+++ spec/mailers/previews/comment_mailer_preview.rb
@@ -1,4 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/comment_mailer
 class CommentMailerPreview < ActionMailer::Preview
+  def notify_user_about_new_comment
+    question = Question.first
+    user = User.first
+    replier = User.last
 
+    CommentMailer.with(question: question,
+                       user: user,
+                       replier: replier)
+                 .notify_user_about_new_comment
+  end
 end
