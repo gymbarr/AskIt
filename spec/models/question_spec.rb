8f44f7dbddfebf6b193bc7f07d1a78ccb63820fe
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-19 15:10:57 +0300

    update factories and associations tests for models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -64,36 +64,31 @@ RSpec.describe Question, type: :model do
     end
   end
 
-  context 'associations' do
-    let(:question) do
-      create :question_with_answers,
-             answers_count: 5,
-             categories_count: 5,
-             subscribers_per_category: 10
-    end
+  describe 'associations' do
+    let(:category) { create :category, :with_subscribers }
+    let(:user) { create :user }
+    let(:question) { create :question, user: user, categories: [category] }
+    let(:answer) { create :answer, repliable: question }
+    let(:question_category) { QuestionCategory.find_by(question: question, category: category) }
 
     it 'has a user' do
-      expect(question.user).to be_instance_of(User)
+      expect(question.user).to eq(user)
     end
 
     it 'has categories' do
-      expect(question.categories.size).to eq(5)
-      expect(question.categories).to all(be_an(Category))
+      expect(question.categories).to contain_exactly(category)
     end
 
     it 'has answers' do
-      expect(question.answers.size).to eq(5)
-      expect(question.answers).to all(be_an(Answer))
+      expect(question.answers).to contain_exactly(answer)
     end
 
     it 'has question_categories' do
-      expect(question.question_categories.size).to eq(5)
-      expect(question.question_categories).to all(be_an(QuestionCategory))
+      expect(question.question_categories).to contain_exactly(question_category)
     end
 
     it 'has subscribers' do
-      expect(question.subscribers.size).to eq(50)
-      expect(question.subscribers).to all(be_an(User))
+      expect(question.subscribers).to contain_exactly(category.subscribers)
     end
   end
 end
