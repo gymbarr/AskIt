7552dfbd0d9c6f8cdd188f6dc5b0d848e798849c
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-19 16:31:49 +0300

    add constants for models validations, optimize notify category subscribers job

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,12 @@
 class Category < ApplicationRecord
+  NAME_MIN_LENGTH = 3
+  NAME_MAX_LENGTH = 40
+
   has_many :question_categories, dependent: :destroy
   has_many :questions, through: :question_categories
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 40 }
+  validates :name, presence: true, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
   validates_uniqueness_of :name
 end
