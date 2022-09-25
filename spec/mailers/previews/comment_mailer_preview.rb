fb97d9fa26225f0f725b990f1d9deddf3ead709f
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-25 22:16:09 +0300

    update mailers previews with factories

--- spec/mailers/previews/comment_mailer_preview.rb
+++ spec/mailers/previews/comment_mailer_preview.rb
@@ -1,9 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/comment_mailer
 class CommentMailerPreview < ActionMailer::Preview
   def notify_user_about_new_comment
-    question = Question.first
-    user = User.first
-    replier = User.last
+    user = User.find_or_create_by(email: 'g-barr@mail.ru') do |new_user|
+      new_user.username = 'Andrey'
+      new_user.password = 'password'
+    end
+
+    replier = User.find_or_create_by(email: 'andrewtoms@mail.ru') do |new_user|
+      new_user.username = 'Andrew Toms'
+      new_user.password = 'password'
+    end
+
+    question = FactoryBot.create(:question, :with_categories, user: user)
 
     CommentMailer.with(question: question,
                        user: user,
--- spec/mailers/previews/question_mailer_preview.rb
+++ spec/mailers/previews/question_mailer_preview.rb
@@ -1,10 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/question_mailer
 class QuestionMailerPreview < ActionMailer::Preview
   def notify_subscriber_about_new_question_in_category
-    question = Question.first
-    user = User.first
-    categories = question.categories
+    user = User.find_or_create_by(email: 'g-barr@mail.ru') do |new_user|
+      new_user.username = 'Andrey'
+      new_user.password = 'password'
+    end
+
+    categories = FactoryBot.create_list(:category, 3)
     categories_name = categories.map(&:name).join(', ')
+    question = FactoryBot.create(:question,
+                                 user: user,
+                                 categories: categories)
 
     QuestionMailer.with(question: question,
                         user: user,
