28270b18d300963b21b814a3e8efa835f8eef5f1
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-28 15:07:18 +0300

    add comment policy

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,20 @@
+class CommentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def update?
+    @user.has_role?(:admin) || @user.author?(record)
+  end
+
+  def destroy?
+    @user.has_role?(:admin) || @user.author?(record)
+  end
+
+  def create?
+    true
+  end
+end
