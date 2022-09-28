02a803eae0269e42ba0bf68a97d2ee336adde184
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-28 13:04:51 +0300

    add pundit gem, user policy, concerns

---
+++ app/controllers/concerns/error_handling.rb
@@ -0,0 +1,13 @@
+module ErrorHandling
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    private
+
+    def record_not_found
+      render plain: '404 Not Found', status: 404
+    end
+  end
+end
