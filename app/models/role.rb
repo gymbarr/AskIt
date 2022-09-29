02a4de4f6f5d39d34c4f378bafd207b388659fc8
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-29 16:42:23 +0300

    remove role column from users table as unused, update user validation for role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,7 @@
 class Role < ApplicationRecord
+  BASIC_USER_ROLE_NAME = :basic
+  ADMIN_USER_ROLE_NAME = :admin
+
   has_and_belongs_to_many :users, join_table: :users_roles
 
   belongs_to :resource,
@@ -10,4 +13,10 @@ class Role < ApplicationRecord
             allow_nil: true
 
   scopify
+
+  class << self
+    def basic_user
+      find_by name: BASIC_USER_ROLE_NAME
+    end
+  end
 end
