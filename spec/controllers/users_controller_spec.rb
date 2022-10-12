f6bdcc12b92079b4f6469ed9d888b3c1b19250a7
Andrey Timakhovich <stewart96.at@gmail.com>
2022-10-12 11:34:55 +0300

    update tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'POST /change_locale' do
-    let(:user) { create :user, locale: 'en' }
+    let(:old_locale) { 'en' }
     let(:new_locale) { 'ru' }
+    let(:user) { create :user, locale: old_locale }
+    subject(:change_locale_request) { post :change_locale, params: { locale: new_locale } }
 
     context 'when authenticated user' do
       it 'changes the users locale' do
         sign_in user
-        post :change_locale, params: { locale: new_locale }
 
-        expect(user.locale).to eq(new_locale)
+        expect { subject }.to change(user, :locale).from(old_locale).to(new_locale)
         expect(response).to have_http_status(:found)
       end
     end
 
-    context 'when authenticated user' do
+    context 'when unauthenticated user' do
       it 'blocks unauthenticated access' do
-        sign_in nil
-        post :change_locale, params: { locale: new_locale }
-
+        expect { subject }.not_to change(user, :locale)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
