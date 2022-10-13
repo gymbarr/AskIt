514de21fdd9729f3ed4f2e1aee52b9047ad19e34
Andrey Timakhovich <stewart96.at@gmail.com>
2022-10-13 17:15:15 +0300

    add bullet gem, configure bullet

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.add_footer = true
+  Bullet.skip_html_injection = false
+end
