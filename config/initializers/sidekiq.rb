7f734a48cc2b82a637f000633c67802fa991af39
Andrey Timakhovich <stewart96.at@gmail.com>
2022-10-19 13:04:35 +0300

    code refactoring for sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
+redis_connection_params = { host: Rails.application.config_for(:redis).host,
+                            port: Rails.application.config_for(:redis).port,
+                            username: Rails.application.config_for(:redis).username,
+                            password: Rails.application.config_for(:redis).password,
+                            database: Rails.application.config_for(:redis).database,
+                            ssl: Rails.application.config_for(:redis).ssl }
+
 Sidekiq.configure_server do |config|
-  config.redis = { host: Rails.application.config_for(:redis).host,
-                   port: Rails.application.config_for(:redis).port,
-                   username: Rails.application.config_for(:redis).username,
-                   password: Rails.application.config_for(:redis).password,
-                   database: Rails.application.config_for(:redis).database,
-                   ssl: Rails.application.config_for(:redis).ssl }
+  config.redis = redis_connection_params
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { host: Rails.application.config_for(:redis).host,
-                   port: Rails.application.config_for(:redis).port,
-                   username: Rails.application.config_for(:redis).username,
-                   password: Rails.application.config_for(:redis).password,
-                   database: Rails.application.config_for(:redis).database,
-                   ssl: Rails.application.config_for(:redis).ssl }
+  config.redis = redis_connection_params
 end
